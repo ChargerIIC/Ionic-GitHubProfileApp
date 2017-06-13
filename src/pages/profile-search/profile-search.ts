0c27a439ce0e59fc310d8db61934b71bce4c6d52
ChargerIIC <ChargerIIC@Yahoo.com>
2017-06-13 12:04:32 -0700

    Initial Page structure

---
+++ src/pages/profile-search/profile-search.ts
@@ -0,0 +1,26 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the ProfileSearchPage page.
+ *
+ * See http://ionicframework.com/docs/components/#navigation for more info
+ * on Ionic pages and navigation.
+ */
+@IonicPage()
+@Component({
+  selector: 'page-profile-search',
+  templateUrl: 'profile-search.html',
+})
+export class ProfileSearchPage {
+
+  username: string = "";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  getUserInformation() : void {
+    this.navCtrl.push('ProfileSearchResultsPage',{ username: this.username})
+  }
+
+}
